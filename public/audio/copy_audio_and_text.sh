d8d011d8994c28a8ea73a3c9f7ff629d403b039a
EricLiu750501 <qazwsxedc123456789eric@gmail.com>
2025-10-14 23:50:06 +0800

    [chore] CMU Audio fix
    選用 bdl (male) 和 clb(female) 的 檔案，挑選約 30 句

---
+++ public/audio/copy_audio_and_text.sh
@@ -0,0 +1,21 @@
+grep -f text_patterns ../../../cmu_us_bdl_arctic > saying.txt
+
+DEST_DIR="cmu_us_bdl_arctic"
+
+mkdir -p "$DEST_DIR"
+
+SRC_DIR='../../../cmu_us_bdl_arctic/wav' 
+
+cat text_patterns \
+| awk -v src_dir="$SRC_DIR" '{print src_dir "/" $1 ".wav"}' \
+| xargs -I {} cp "{}" "$DEST_DIR"
+
+DEST_DIR="cmu_us_clb_arctic"
+mkdir -p "$DEST_DIR"
+
+SRC_DIR='../../../cmu_us_clb_arctic/wav' 
+
+cat text_patterns \
+| awk -v src_dir="$SRC_DIR" '{print src_dir "/" $1 ".wav"}' \
+| xargs -I {} cp "{}" "$DEST_DIR"
+
